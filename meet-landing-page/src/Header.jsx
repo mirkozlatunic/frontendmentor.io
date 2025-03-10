3ec52c8d77957778cfc88e3143ccaf919da4d51c
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-03-09 22:22:30 -0700

    adding header logo

--- meet-landing-page/src/Header.jsx
+++ meet-landing-page/src/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import logo from "./assets/logo.svg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header className="flex w-full items-center justify-between px-8.5 py-8 md:px-15">
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
 }
 
 export default Header;
