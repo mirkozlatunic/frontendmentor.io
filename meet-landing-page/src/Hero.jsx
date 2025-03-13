84d50e654c5999085cfbf9d0d773a9619fb17e40
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-03-12 19:43:34 -0700

    section number component completed

--- meet-landing-page/src/Hero.jsx
+++ meet-landing-page/src/Hero.jsx
@@ -3,12 +3,12 @@ import imgCircles from "./assets/tablet/image-hero.png";
 
 function Hero() {
   return (
-    <div className="align-center flex w-full flex-col items-center pt-16 text-center">
+    <div className="align-center flex w-full flex-col items-center p-8 pt-16 text-center">
       <div className="pb-12">
         <img
           src={imgCircles}
           alt="circles"
-          className="max-w-md overflow-x-auto"
+          className="max-w-[433px] overflow-x-auto"
         />
       </div>
       <div className="flex flex-col items-center gap-6">
@@ -17,10 +17,11 @@ function Hero() {
           Meet makes it easy to connect with others face-to-face virtually and
           collaborate across any device.
         </p>
-        <button className="w-[193px] rounded-full bg-[#4d96a9] px-10 py-4 font-[900] text-white">
-          Download <span className="text-[#8fe3f9]">v1.3</span>
+        <button className="w-[193px] cursor-pointer rounded-full bg-[#4d96a9] px-10 py-4 font-[900] text-white hover:bg-[#71c0d4]">
+          Download{" "}
+          <span className="text-[#8fe3f9] hover:text-[#71c0d4]">v1.3</span>
         </button>
-        <button className="rounded-full bg-[#855fb1] px-10 py-4 font-[900] text-white">
+        <button className="cursor-pointer rounded-full bg-[#855fb1] px-10 py-4 font-[900] text-white hover:bg-[#b18bdd]">
           What is it
         </button>
       </div>
--- meet-landing-page/src/Section1.jsx
+++ meet-landing-page/src/Section1.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Section1() {
-  return <div>Section1</div>;
+  return (
+    <div className="flex flex-col items-center p-8">
+      <div className="h-20 w-[2px] bg-[#d1d1df]"></div>
+      <p className="rounded-full border-2 border-[#d1d1df] px-5 py-4 font-[900] text-[#87879d]">
+        01
+      </p>
+    </div>
+  );
 }
 
 export default Section1;
