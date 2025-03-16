4d71bfa8fb81b643daf414bb16f86036a3696c53
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-03-16 13:32:57 -0700

    first phase of footer completed

--- meet-landing-page/src/Footer.jsx
+++ meet-landing-page/src/Footer.jsx
@@ -10,17 +10,24 @@ function Footer() {
           02
         </p>
       </div>
-      <div className="-mt-7 bg-[#4d96a9]">
-        <img src={imgFooter} className="opacity-10" />
-      </div>
-      <div className="text-center">
-        <h2 className="pb-8 text-3xl font-[900] text-[#28283d]">
+      {/* <div className="-mt-7">
+        <img
+          src={imgFooter}
+          className="absolute left-0 max-h-[374px] w-full opacity-10"
+        />
+      </div> */}
+      <div className="-mt-7 flex flex-col items-center justify-center gap-6 bg-[#4d96a9] px-8 py-16 text-center">
+        <h2 className="text-3xl font-[900] text-white">
           Experience more together
         </h2>
-        <p className="abso text-[#87879d]">
+        <p className="text-white">
           Stay connected with reliable HD meetings and unlimited one-on-one and
           group video sessions.
         </p>
+        <button className="w-[193px] cursor-pointer rounded-full bg-[#855fb1] px-10 py-4 font-[900] text-white hover:bg-[#b18bdd]">
+          Download{" "}
+          <span className="text-purple-300 hover:text-purple-200">v1.3</span>
+        </button>
       </div>
     </footer>
   );
