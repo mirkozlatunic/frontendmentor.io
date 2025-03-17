835942d5fbf93646f0c125fefb6c88bec81b99dc
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-03-17 10:43:43 -0700

    footer tablet completed

--- meet-landing-page/src/Footer.jsx
+++ meet-landing-page/src/Footer.jsx
@@ -13,20 +13,22 @@ function Footer() {
       <img
         src={imgFooter}
         alt="Footer Image"
-        className="absolute mt-28 h-[374px] object-cover opacity-15"
+        className="absolute mt-28 h-[374px] object-cover opacity-15 md:h-[320px]"
       />
-      <div className="-mt-7 flex h-[374px] flex-col items-center justify-center gap-6 bg-[#4d96a9] px-8 py-16 text-center">
-        <h2 className="text-3xl font-[900] text-white">
-          Experience more together
-        </h2>
-        <p className="text-white">
-          Stay connected with reliable HD meetings and unlimited one-on-one and
-          group video sessions.
-        </p>
-        <button className="z-10 w-[193px] cursor-pointer rounded-full bg-[#855fb1] px-10 py-4 font-[900] text-white hover:bg-[#b18bdd]">
-          Download{" "}
-          <span className="text-purple-300 hover:text-purple-200">v1.3</span>
-        </button>
+      <div className="-mt-7 flex h-[374px] flex-col items-center bg-[#4d96a9] px-8 py-16 text-center md:h-[320px] md:w-screen">
+        <div className="flex flex-col items-center gap-6 md:w-[504px]">
+          <h2 className="text-3xl font-[900] text-white">
+            Experience more together
+          </h2>
+          <p className="text-white">
+            Stay connected with reliable HD meetings and unlimited one-on-one
+            and group video sessions.
+          </p>
+          <button className="z-10 w-[193px] cursor-pointer rounded-full bg-[#855fb1] px-10 py-4 font-[900] text-white hover:bg-[#b18bdd]">
+            Download{" "}
+            <span className="text-purple-300 hover:text-purple-200">v1.3</span>
+          </button>
+        </div>
       </div>
     </footer>
   );
