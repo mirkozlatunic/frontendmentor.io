bae2030f7c9b4ffcb745384a822f7a44be1dfc22
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-01 22:24:59 -0700

    setup of first card prototype

--- newsletter-signup/src/App.jsx
+++ newsletter-signup/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
     <>
-      <h1 className="font-roboto container mx-auto flex min-h-screen flex-col items-center justify-center">
-        Hello word
+      <h1 className="font-roboto container mx-auto flex min-h-screen flex-col items-center justify-center bg-[#36384D]">
+        <Card />
       </h1>
     </>
   );
---
+++ newsletter-signup/src/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="rounded-5xl max-h- max-h-[605px] max-w-[905px] bg-white p-8">
+      <div></div>
+    </div>
+  );
+}
+
+export default Card;
