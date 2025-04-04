301c0970bef59e817d4474099e937596fc39d834
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-03 21:58:34 -0700

    card mobile tet completed

--- newsletter-signup/src/App.jsx
+++ newsletter-signup/src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 function App() {
   return (
     <>
-      <h1 className="font-roboto container mx-auto flex min-h-screen min-w-screen flex-col items-center md:justify-center md:bg-[#36384D]">
+      <h1 className="font-roboto container mx-auto flex min-h-screen min-w-screen flex-col items-center text-[#242742] md:justify-center md:bg-[#36384D]">
         <Card />
       </h1>
     </>
--- newsletter-signup/src/Card.jsx
+++ newsletter-signup/src/Card.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import img from "./assets/images/illustration-sign-up-mobile.svg";
+import checkmark from "./assets/images/icon-list.svg";
+import Form from "./Form";
+
+const bullets = [
+  "Product discovery and building what matters",
+  "Measuring to ensure updates are a success",
+  "And much more!",
+];
 
 function Card() {
   return (
-    <div className="">
+    <div className="pb-10">
       <img src={img} alt="image" />
-      <div></div>
+      <div className="px-6 py-12">
+        <h1 className="pb-6 text-5xl font-bold">Stay update!</h1>
+        <p className="pb-6">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+        <ul>
+          {bullets.map((bullet) => (
+            <div className="flex gap-4 pb-2">
+              <img src={checkmark} alt="checkmark" />
+              <li>{bullet}</li>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <Form />
     </div>
   );
 }
