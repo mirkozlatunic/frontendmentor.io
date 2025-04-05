1d495d98db8c1f2803abd7956714b09fdac4b9a5
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-04 21:52:41 -0700

    start form component

--- newsletter-signup/src/Form.jsx
+++ newsletter-signup/src/Form.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Form() {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Email adress</label>
+        <input
+          type="email"
+          placehorder="ash@loremcompany.com"
+          className="border-2"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Form;
