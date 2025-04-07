cd1d2acf357259cb6723a49a81076b303ff5d834
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-06 21:31:13 -0700

    first setup of validation for emial

--- newsletter-signup/src/Form.jsx
+++ newsletter-signup/src/Form.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import { useState } from "react";
+
+const isEmail = (email) =>
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email);
 
 function Form() {
+  const [value, setValue] = useState({ email: "" });
+  const [errors, setErrors] = useState({});
+
+  const validateAndSubmitForm = (e) => {
+    e.preventDefault();
+
+    const errors = {};
+
+    if (!isEmail(value.email)) {
+      errors.email = "Valid email required";
+    }
+
+    setErrors(errors);
+
+    if (!Object.keys(errors).length) {
+      alert(JSON.stringify(value, null, 2));
+    }
+
+    const setEmail = (e) => {
+      setValue({ ...value, email: e.target.value });
+    };
+  };
+
   return (
     <div className="flex flex-col justify-center gap-6 px-6 md:px-0">
-      <form action="" className="flex flex-col gap-2">
+      <form action="" className="flex flex-col gap-2" onSubmit={handleSubmit}>
         <label htmlFor="" className="font-bold">
           Email address
         </label>
