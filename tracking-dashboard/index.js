2419486c1ec0fc8a5b2b63942ac39e5afbda4ef4
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-04-16 13:28:00 -0700

    desktop version completed

--- tracking-dashboard/index.js
+++ tracking-dashboard/index.js
@@ -1,28 +1,45 @@
-function createCard(cardData) {
-  const card = document.createElement('div');
-  card.className = 'card';
+const buttonEl = document.querySelector('.date-card-button');
 
-  card.innerHTML = `
-  <div class="card">
-        <div class="img-card">
-          <img src="./images/icon-work.svg" alt="work" class="img-bg" />
-        </div>
-        <div class="time-card">
-          <div class="time-card-header">
-            <p class="time-card-section">${cardData.title}</p>
-            <img
-              src="./images/icon-ellipsis.svg"
-              alt="dots"
-              class="time-card-dots"
-            />
-          </div>
-          <div class="time-card-data">
-            <p class="time-card-hours">${cardData.current}hrs</p>
-            <p class="time-card-week">Last Week - ${cardData.previous}hrs</p>
-          </div>
-        </div>
-      </div>
-  `;
+// Fetch data from the JSON file
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    renderCards(data);
+  })
+  .catch((error) => console.error('Error fetching data:', error));
 
-  return card;
+// Function to render cards
+function renderCards(data) {
+  const container = document.querySelector('.cards-container');
+
+  data.forEach((item) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+      <div class="card">
+          <div class="img-card" style="background-color: ${item.backgroundColor}">
+            <img src=${item.image} alt="${item.title}" class="img-bg" />
+          </div>
+          <div class="time-card">
+            <div class="time-card-header">
+              <p class="time-card-section">${item.title}</p>
+              <img
+                src="./images/icon-ellipsis.svg"
+                alt="dots"
+                class="time-card-dots"
+              />
+            </div>
+            <div class="time-card-data">
+              <p class="time-card-hours">${item.timeframes.weekly.current}hrs</p>
+              <p class="time-card-week">Last Week - ${item.timeframes.weekly.previous}hrs</p>
+            </div>
+          </div>
+        </div>
+    `;
+
+    container.appendChild(card);
+  });
 }
+
+function dateChange() {}
