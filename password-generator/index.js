9e02f2246f1885807dd93982966f2e79f0bb6888
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-05-04 22:08:38 -0700

    slider styling and function completed

--- password-generator/index.js
+++ password-generator/index.js
@@ -2,10 +2,20 @@
 let copyButton = document.querySelector('.copy-btn');
 let copyText = document.querySelector('.copy-text');
 let passwordText = document.querySelector('.password');
+let slider = document.querySelector('.slider');
+let charLength = document.querySelector('.length-value');
 
 function copyPassword() {
   copyText.textContent = 'COPIED';
   navigator.clipboard.writeText(passwordText.textContent);
 }
+
+slider.oninput = function () {
+  charLength.textContent = this.value;
+
+  // Update the slider's background
+  const value = ((this.value - this.min) / (this.max - this.min)) * 100; // Calculate percentage
+  this.style.background = `linear-gradient(to right, #a4ffaf ${value}%, #18171f ${value}%)`;
+};
 
 copyButton.addEventListener('click', copyPassword);
