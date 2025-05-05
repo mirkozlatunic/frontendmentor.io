8c45cfd434d35a216727c2a0e5a18b8dab0a29d0
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-05-04 21:50:45 -0700

    password copy function is working

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,1 +1,11 @@
 // Password generator functionality will be implemented later
+let copyButton = document.querySelector('.copy-btn');
+let copyText = document.querySelector('.copy-text');
+let passwordText = document.querySelector('.password');
+
+function copyPassword() {
+  copyText.textContent = 'COPIED';
+  navigator.clipboard.writeText(passwordText.textContent);
+}
+
+copyButton.addEventListener('click', copyPassword);
